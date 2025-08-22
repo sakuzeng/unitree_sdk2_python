d4693cc4b78223c8f7047d91c5ac7e71bea2ae9d
sakuzeng <sakuzeng@foxmail.com>
2025-08-22 18:33:04 +0800

    code:g1_loco_client_test

--- unitree_sdk2py/g1/loco/g1_loco_client.py
+++ unitree_sdk2py/g1/loco/g1_loco_client.py
@@ -31,6 +31,7 @@ class LocoClient(Client):
         self._RegistApi(ROBOT_API_ID_LOCO_SET_ARM_TASK, 0)
 
     # 7101
+    # 设置 FSM（有限状态机）ID，控制机器人运动模式。 
     def SetFsmId(self, fsm_id: int):
         p = {}
         p["data"] = fsm_id
@@ -39,6 +40,7 @@ class LocoClient(Client):
         return code
 
     # 7102
+    # 设置机器人平衡模式。
     def SetBalanceMode(self, balance_mode: int):
         p = {}
         p["data"] = balance_mode
@@ -47,6 +49,7 @@ class LocoClient(Client):
         return code
 
     # 7104
+    # 设置站立高度。
     def SetStandHeight(self, stand_height: float):
         p = {}
         p["data"] = stand_height
@@ -55,6 +58,7 @@ class LocoClient(Client):
         return code
 
     # 7105
+    # 设置机器人速度（前进、侧移、旋转），可指定持续时间。
     def SetVelocity(self, vx: float, vy: float, omega: float, duration: float = 1.0):
         p = {}
         velocity = [vx,vy,omega]
@@ -65,55 +69,75 @@ class LocoClient(Client):
         return code
     
     # 7106
+    # 设置机械臂任务 ID
     def SetTaskId(self, task_id: float):
         p = {}
         p["data"] = task_id
         parameter = json.dumps(p)
         code, data = self._Call(ROBOT_API_ID_LOCO_SET_ARM_TASK, parameter)
         return code
-
+    
+    # 8001
+    # 获得FSM（有限状态机）ID
+    def GetFsmId(self):
+        code, data = self._Call(ROBOT_API_ID_LOCO_GET_FSM_ID, "")
+        if code == 0:
+            return data
+        return None
+    # 8002
+    # 获得FSM（有限状态机）模式
+    def GetFsmMode(self):
+        code, data = self._Call(ROBOT_API_ID_LOCO_GET_FSM_MODE, "")
+        if code == 0:
+            return data
+        return None
+    # 零力矩模式
+    def ZeroTorque(self):
+        self.SetFsmId(0)
+    # 阻尼模式
     def Damp(self):
         self.SetFsmId(1)
-    
+    # 站立模式
+    def StandUp(self):
+        self.SetFsmId(4)
+    # 主运控模式
     def Start(self):
         self.SetFsmId(200)
-
+    # 蹲下到站立
     def Squat2StandUp(self):
         self.SetFsmId(706)
-
+    # 躺下到站立
     def Lie2StandUp(self):
         self.SetFsmId(702)
-
+    # 坐
     def Sit(self):
         self.SetFsmId(3)
-
+    # 站立到蹲下
     def StandUp2Squat(self):
         self.SetFsmId(706)
 
-    def ZeroTorque(self):
-        self.SetFsmId(0)
-
+    # 停止移动
     def StopMove(self):
         self.SetVelocity(0., 0., 0.)
-
+    # 最高站立
     def HighStand(self):
         UINT32_MAX = (1 << 32) - 1
         self.SetStandHeight(UINT32_MAX)
-
+    # 最低站立
     def LowStand(self):
         UINT32_MIN = 0
         self.SetStandHeight(UINT32_MIN)
-
+    # 移动（前进、侧移、旋转），可选持续移动。
     def Move(self, vx: float, vy: float, vyaw: float, continous_move: bool = False):
         duration = 864000.0 if continous_move else 1
         self.SetVelocity(vx, vy, vyaw, duration)
-
+    # 平衡站立。
     def BalanceStand(self, balance_mode: int):
         self.SetBalanceMode(balance_mode)
-
+    # 挥手（可选转身）。
     def WaveHand(self, turn_flag: bool = False):
         self.SetTaskId(1 if turn_flag else 0)
-
+    # 握手动作
     def ShakeHand(self, stage: int = -1):
         if stage == 0:
             self.first_shake_hand_stage_ = False
